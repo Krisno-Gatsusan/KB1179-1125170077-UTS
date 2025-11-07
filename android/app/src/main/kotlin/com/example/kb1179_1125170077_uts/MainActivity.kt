350f07b625f1bfeddd128aa4c23cc29ff71b5c25
Krisno-Gatsusan <kgatsusan@gmail.com>
2025-11-07 20:12:16 +0700

    membuat void main dan override

---
+++ android/app/src/main/kotlin/com/example/kb1179_1125170077_uts/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kb1179_1125170077_uts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyMaterialApp());
+}
+
+class MyMaterialApp extends StatelessWidget {
+  const MyMaterialApp({super.key});
+}
