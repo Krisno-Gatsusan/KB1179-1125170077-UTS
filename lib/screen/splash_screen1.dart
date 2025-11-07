7b0c3c34224399eeda9b21b6c05957d89dbeb492
Krisno-Gatsusan <kgatsusan@gmail.com>
2025-11-07 20:44:42 +0700

    membuat titik tiga

--- lib/screen/splash_screen1.dart
+++ lib/screen/splash_screen1.dart
@@ -26,10 +26,51 @@ class Mysplashscreen extends StatelessWidget {
           Text(
             "Welcome",
             style: TextStyle(
-              fontSize: 32.0,
+              fontSize: 40.0,
               color: Colors.deepPurple,
               fontWeight: FontWeight.bold,
             ),
+          ),
+          Text(
+            "Forgot to bring your wallet \n when you are shopping? ",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.deepPurple,
+                ),
+              ),
+              SizedBox(width: 5),
+              Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(width: 5),
+              Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
           ),
         ],
       ),
