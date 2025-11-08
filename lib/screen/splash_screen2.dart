bab0116e53d5eaa07c8e6a77947db6df33b19659
Krisno-Gatsusan <kgatsusan@gmail.com>
2025-11-08 08:07:40 +0700

    pembuatan isi dari splasscreen 2

--- lib/screen/splash_screen2.dart
+++ lib/screen/splash_screen2.dart
@@ -1,1 +1,103 @@
+import 'package:flutter/material.dart';
 
+class Mysplashscreen2 extends StatelessWidget {
+  const Mysplashscreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 20),
+          Container(
+            height: 240,
+            width: 240,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.deepPurple,
+              image: DecorationImage(
+                image: AssetImage("../assets/images/labubu1.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "Forgot to bring your wallet \n when you are shopping? ",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(width: 5),
+              Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.deepPurple,
+                ),
+              ),
+              SizedBox(width: 5),
+              Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          Container(
+            margin: EdgeInsets.only(left: 40, right: 40),
+            child: SizedBox(
+              height: 40,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  //Todo call next pages
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                ),
+                child: Text(
+                  "Continue",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
